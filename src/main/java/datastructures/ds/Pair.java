24790d7832bd89b0c3f203e262f968b316cf7f88
jeffreywangcf <jeffreywangcf@gmail.com>
2023-07-03 12:39:32 -0400

    added javadoc for Pair

--- src/main/java/datastructures/ds/Pair.java
+++ src/main/java/datastructures/ds/Pair.java
@@ -1,30 +1,61 @@
 package datastructures.ds;
 
+
+/**
+ * represents a pair of data
+ * @param <K> type of first
+ * @param <V> type of second
+ */
 public class Pair<K, V>{
     K first;
     V second;
 
+    /**
+     * default constructor
+     * @param first data of first
+     * @param second data of second
+     */
     public Pair(K first, V second){
         this.first = first;
         this.second = second;
     }
 
+    /**
+     * getter for first
+     * @return first
+     */
     public K getFirst(){
         return this.first;
     }
 
+    /**
+     * setter for first
+     * @param first new value
+     */
     public void setFirst(K first){
         this.first = first;
     }
 
+    /**
+     * getter for second
+     * @return second
+     */
     public V getSecond(){
         return this.second;
     }
 
+    /**
+     * setter for second
+     * @param second second
+     */
     public void setSecond(V second){
         this.second = second;
     }
 
+    /**
+     * convert pair to a string
+     * @return string representation 
+     */
     @Override
     public String toString(){
         return String.format("(%s: %s) ", this.first.toString(), this.second.toString());
